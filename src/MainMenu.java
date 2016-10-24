b5abd5d1c0cc84b1631249e4c4a99131bfb354d0
cmundosa <cmundosa@asu.edu>
2016-10-24 16:44:06 -0700

    Celso's version of TestInterface

---
+++ src/MainMenu.java
@@ -0,0 +1,26 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class MainMenu extends JFrame {
+    private JButton jbtCreateGame = new JButton("Create Game");
+    private JButton jbtJoinGame = new JButton("Join Game");
+    private JButton jbtBack = new JButton("back");
+    private JButton jbtContinue = new JButton("continue");
+    JPanel pnlHolder;
+
+    public MainMenu(){
+        Box box = Box.createVerticalBox();
+        box.add(jbtCreateGame);
+        box.add(jbtJoinGame);
+        add(box);
+    }
+
+    public static void main(String[] args) {
+        MainMenu frame = new MainMenu();
+        frame.setTitle("Bird Race");
+        frame.setSize(500,500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
