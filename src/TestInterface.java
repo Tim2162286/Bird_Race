e37c8cac3d8e620891f53af55d5b3259f146e367
Tim2162286 <tjcuprak@gmail.com>
2016-10-24 16:18:56 -0700

    Created test class for GUI

---
+++ src/TestInterface.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+/**
+ * Created by Tim on 10/24/2016.
+ */
+public class TestInterface {
+    public static void main(String[] argv) {
+        JFrame mainMenu = new JFrame("Main Menu");
+        mainMenu.setSize(300,300);
+    }
+
+
+}
