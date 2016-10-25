05d39e403e81da378fedfc7531c54c4c12f3f0c4
JonathanBush <buoywestone@gmail.com>
2016-10-24 23:08:06 -0700

    Implemented some of the server commands.

--- src/ServerThread.java
+++ src/ServerThread.java
@@ -9,9 +9,13 @@ import java.net.Socket;
 public class ServerThread implements Runnable {
 
     private Socket playerSocket;
+    private int playerId;
+    private String handle;
 
     public ServerThread(Socket playerSocket) {
         this.playerSocket = playerSocket;
+        this.playerId = (int)(Math.random() * Integer.MAX_VALUE);
+        this.handle = "default";
     }
 
     @Override
@@ -20,13 +24,45 @@ public class ServerThread implements Runnable {
             PrintWriter out = new PrintWriter(playerSocket.getOutputStream(), true);
             BufferedReader in = new BufferedReader(new InputStreamReader(playerSocket.getInputStream()));
             String inputLine, outputLine;
-            while ((inputLine = in.readLine()) != null) {
-                outputLine = "echo: " + inputLine;
-                out.println(outputLine);
-                System.out.println(inputLine);
-                if (inputLine.equals("Bye")) {
-                    break;
+            boolean disconnect = false;
+            while (!disconnect && (inputLine = in.readLine()) != null) {
+                String[] command = inputLine.split("\\s+");
+                String response = null;
+                switch (command[0]) {
+                    case "status":
+                        response = "not_implemented";
+                        break;
+                    case "setid":
+                        try {
+                            this.playerId = Integer.parseInt(command[1]);
+                            response = "valid";
+                        } catch (IndexOutOfBoundsException | NumberFormatException e) {
+                            System.out.println("Exception at clientid command: " + e);
+                            response = "invalid";
+                        }
+                        break;
+
+                    case "handle":
+                        try {
+                            this.handle = command[1];
+                            response = "valid";
+                        } catch (IndexOutOfBoundsException e) {
+                            System.out.println("Error at handle command " + e);
+                            response = "invalid";
+                        }
+                        break;
+
+                    case "disconnect":
+                        disconnect = true;
+                        response = "closing";
+                        break;
+
+                    default:
+                        response = "invalid_command";
+
                 }
+                out.println(response);
+                //System.out.println(inputLine);
             }
         }  catch (Throwable e) {}
     }
