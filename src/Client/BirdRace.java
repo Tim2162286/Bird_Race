012eff7536c02280f7488d5d0ffab844c940df11
JonathanBush <buoywestone@gmail.com>
2016-10-31 19:03:55 -0700

    Moved game functionality to the GamePanel.

--- src/Client/BirdRace.java
+++ src/Client/BirdRace.java
@@ -3,9 +3,8 @@ package Client;
 /**
  * Created by Celso on 10/30/2016.
  */
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.Graphics;
+import java.awt.*;
+
 import javafx.scene.shape.Circle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -13,86 +12,30 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import javax.swing.*;
 
-public class BirdRace extends JPanel implements ActionListener, KeyListener {
-    public static BirdRace birdrace;
+public class BirdRace {
+    public static GamePanel birdracepanel;
     public final int WIDTH = 1280;
     public final int HEIGHT = 720;
-    //public Renderer renderer;
-    public Circle bird;
-    public int press;
-    public int yMotion;
+    //public Renderer renderer
+
+    private static final int UPDATE_DELAY = 25;     // Time between updates in ms
 
     public BirdRace(){
         JFrame frame = new JFrame();
-        //renderer = new Renderer();
-        Timer timer = new Timer(25,this);
 
-        //frame.add(renderer);
-        frame.add(this);
+        frame.add(new GamePanel());
         frame.setTitle("BirdRace");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setSize(WIDTH, HEIGHT);
-        frame.addKeyListener(this);
         frame.setResizable(false);
         frame.setVisible(true);
-
+        frame.addKeyListener(birdracepanel);
         //Creates bird and sets size
-        bird = new Circle(WIDTH/4-10, HEIGHT/2-10, 30);
-        timer.start();
-        this.repaint();
     }
 
 
 
     public static void main(String[] args){
-        birdrace = new BirdRace();
-    }
-
-    //Moves bird in y direction when spacebar is pressed
-    public void flap(){
-        if (yMotion > 0){
-            yMotion = 0;
-        }
-        yMotion = yMotion - 10;
-    }
-    @Override
-    public void actionPerformed(ActionEvent e){
-        press++;
-        if (press % 2 == 0){
-            yMotion += 2;
-        }
-        bird.setCenterY(bird.getCenterY() + yMotion);
-
-        this.repaint();
-    }
-    @Override
-    public void keyReleased(KeyEvent e){
-        if (e.getKeyCode() == KeyEvent.VK_SPACE)
-        {
-            flap();
-        }
-    }
-    @Override
-    public void keyTyped(KeyEvent e)
-    {
-
-    }
-    @Override
-    public void keyPressed(KeyEvent e)
-    {
-
-    }
-
-    @Override
-    public void paintComponent(Graphics g){
-        super.paintComponent(g);
-        g.setColor(Color.cyan);
-        g.fillRect(0, 0, WIDTH, HEIGHT);
-
-        g.setColor(Color.orange);
-        g.fillRect(0, HEIGHT - 120, WIDTH, 120);
-
-        g.setColor(Color.yellow);
-        g.fillOval((int)bird.getCenterX(), (int)bird.getCenterY(), 60, 60);
+        new BirdRace();
     }
 }
---
+++ src/Client/GamePanel.java
@@ -0,0 +1,90 @@
+package Client;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+
+/**
+ * Created by jon on 10/28/2016.
+ */
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import javafx.scene.shape.Circle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class GamePanel extends JPanel implements ActionListener, KeyListener {
+    public final int WIDTH = 1280;
+    public final int HEIGHT = 720;
+    //public Renderer renderer;
+    public Circle bird;
+    public int press;
+    public int yMotion;
+
+    private static final int UPDATE_DELAY = 25;
+
+    public GamePanel(){
+        Timer timer = new Timer(UPDATE_DELAY, this);
+
+        //Creates bird and sets size
+        bird = new Circle(WIDTH/4-10, HEIGHT/2-10, 30);
+        //this.addKeyListener(this);
+        this.setFocusable(true);
+        this.requestFocusInWindow();
+        this.addKeyListener(this);
+        timer.start();
+        this.repaint();
+    }
+
+
+    //Moves bird in y direction when spacebar is pressed
+    public void flap(){
+        if (yMotion > 0){
+            yMotion = 0;
+        }
+        yMotion = yMotion - 10;
+    }
+    @Override
+    public void actionPerformed(ActionEvent e){
+        press++;
+        if (press % 2 == 0){
+            yMotion += 2;
+        }
+        bird.setCenterY(bird.getCenterY() + yMotion);
+
+        this.repaint();
+    }
+    @Override
+    public void keyReleased(KeyEvent e){
+        if (e.getKeyCode() == KeyEvent.VK_SPACE)
+        {
+            flap();
+        }
+    }
+    @Override
+    public void keyTyped(KeyEvent e)
+    {
+
+    }
+    @Override
+    public void keyPressed(KeyEvent e)
+    {
+
+    }
+
+    @Override
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        g.setColor(Color.cyan);
+        g.fillRect(0, 0, WIDTH, HEIGHT);
+
+        g.setColor(Color.orange);
+        g.fillRect(0, HEIGHT - 120, WIDTH, 120);
+
+        g.setColor(Color.yellow);
+        g.fillOval((int)bird.getCenterX(), (int)bird.getCenterY(), 60, 60);
+    }
+}
