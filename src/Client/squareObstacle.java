f560fbdfa7f5e190433f92bb18076b4e13c6ef12
Tim2162286 <tjcuprak@gmail.com>
2016-11-06 19:14:41 -0700

    Renamed squareObstacle to SquareObstacle

---
+++ src/Client/squareObstacle.java
@@ -0,0 +1,24 @@
+package Client;
+
+import java.awt.*;
+import java.awt.geom.Rectangle2D;
+import java.util.Random;
+
+/**
+ * Created by Tim on 11/6/2016.
+ */
+public class SquareObstacle extends ObstacleMasterClass {
+    private Rectangle2D topShape;
+    private Rectangle2D bottomShape;
+    public SquareObstacle(Random random, int windowHeight, int bottomHeight, int updateDelay){
+        super(random, windowHeight, bottomHeight, updateDelay);
+        topShape = new Rectangle2D.Double(super.xPos,0,super.width, super.topHeight);
+        bottomShape = new Rectangle2D.Double(super.xPos,super.bottomHeight,super.width,super.HEIGHT-super.BOTTOM_HEIGHT-super.bottomHeight);
+    }
+
+    @Override
+    public void paint(Graphics2D graphics) {
+        graphics.fill(topShape);;
+        graphics.fill(bottomShape);
+    }
+}
