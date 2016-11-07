c7ca5ebc14a23fad95465319802b222e29ea9212
Tim2162286 <tjcuprak@gmail.com>
2016-11-06 20:48:55 -0700

    Obstacles move, crashes when first one is removed.

--- src/Client/squareObstacle.java
+++ src/Client/squareObstacle.java
@@ -10,8 +10,8 @@ import java.util.Random;
 public class SquareObstacle extends ObstacleMasterClass {
     private Rectangle2D topShape;
     private Rectangle2D bottomShape;
-    public SquareObstacle(Random random, int windowHeight, int bottomHeight, int updateDelay){
-        super(random, windowHeight, bottomHeight, updateDelay);
+    public SquareObstacle(Random random, int windowHeight, int bottomHeight, int updateDelay, int farthest){
+        super(random, windowHeight, bottomHeight, updateDelay, farthest);
         topShape = new Rectangle2D.Double(super.xPos,0,super.width, super.topHeight);
         bottomShape = new Rectangle2D.Double(super.xPos,super.bottomHeight,super.width,super.HEIGHT-super.BOTTOM_HEIGHT-super.bottomHeight);
     }
@@ -20,5 +20,14 @@ public class SquareObstacle extends ObstacleMasterClass {
     public void paint(Graphics2D graphics) {
         graphics.fill(topShape);;
         graphics.fill(bottomShape);
+    }
+    @Override
+    public void update() {
+        super.update();
+        topShape.setFrame(super.xPos,0,super.width, super.topHeight);
+        bottomShape.setFrame(super.xPos,super.bottomHeight,super.width,super.HEIGHT-super.BOTTOM_HEIGHT-super.bottomHeight);
+    }
+    public boolean remove(){
+        return super.remove();
     }
 }
