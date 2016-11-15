5017ff4f59e5aa5637d89e053cb8e0391ebedb4d
Tim2162286 <tjcuprak@gmail.com>
2016-11-14 21:13:37 -0700

    Changed ObstacleCount to be a passed parameter.

--- src/Client/GamePanel.java
+++ src/Client/GamePanel.java
@@ -18,7 +18,7 @@ import java.util.Random;
 
 public class GamePanel extends JPanel implements ActionListener, KeyListener {
     private Random rand;
-    private final int OBSTACAL_COUNT = 20;
+    private int OBSTACLE_COUNT;
     private final int WIDTH = 1280;
     private final int HEIGHT = 720;
     private static final int UPDATE_DELAY = 17;
@@ -55,7 +55,8 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener {
         return leaders;
     }
 
-    public GamePanel(String name){
+    public GamePanel(String name, int obstacleCount){
+        OBSTACLE_COUNT = obstacleCount;
         this.name = name;
         try{
              client = new ClientMaster();
@@ -68,13 +69,13 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener {
             }
             catch(InterruptedException e){}
         }
-        client.setHandle("Tim");
+        client.setHandle(this.name);
         client.requestGameId();
         client.requestPlayerId();
         client.requestHandles();
         client.requestObstaclesPassed();
         rand = new Random(client.getGameId());
-        bird = new Bird(rand, WIDTH, HEIGHT, BOTTOM_HEIGHT, UPDATE_DELAY,OBSTACAL_COUNT);
+        bird = new Bird(rand, WIDTH, HEIGHT, BOTTOM_HEIGHT, UPDATE_DELAY, OBSTACLE_COUNT);
         while (client.backlog()){System.out.println("hi");}
         id = client.getPlayerId();
         playerNameList = client.getHandles();
@@ -138,11 +139,11 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener {
         g.fillRect(WIDTH/4, HEIGHT - BOTTOM_HEIGHT + 5, WIDTH/2, BOTTOM_HEIGHT);
 
         for (int i=0;i<playerNameList.length;i++) {
-            if (playerNameList[i].equals(name))
+            if (i==id)
                 g.setColor(Color.red);
             else
                 g.setColor(Color.black);
-            g.fillOval(WIDTH/4+1+playerScoreList[i]*(WIDTH/2-42)/OBSTACAL_COUNT,HEIGHT-BOTTOM_HEIGHT+i*11+4  , 10, 10);
+            g.fillOval(WIDTH/4+1+playerScoreList[i]*(WIDTH/2-42)/ OBSTACLE_COUNT,HEIGHT-BOTTOM_HEIGHT+i*11+4  , 10, 10);
         }
 
 
