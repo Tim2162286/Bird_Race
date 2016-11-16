245355a6a1a27292b4ea6a01603f1b093696f55d
JonathanBush <buoywestone@gmail.com>
2016-11-16 15:09:02 -0700

    Added name formatter with censoring of bad words.

--- src/Client/NameFormatter.java
+++ src/Client/NameFormatter.java
@@ -5,14 +5,14 @@ package Client;
  */
 public final class NameFormatter {
 
-    public static final CharSequence[] censored = {"anal","anus","arse","ass","ballsack","balls","bastard","bitch","biatch","bloody","blowjob","blow job","bollock","bollok","boner","boob","bugger","bum","butt","buttplug","clitoris","cock","coon","crap","cunt","damn","dick","dildo","dyke","fag","feck","fellate","fellatio","felching","fuck","f u c k","fudgepacker","fudge packer","flange","Goddamn","God damn","hell","homo","jerk","jizz","knobend","knob end","labia","lmao","lmfao","muff","nigger","nigga","omg","penis","piss","poop","prick","pube","pussy","queer","scrotum","sex","shit","s hit","sh1t","slut","smegma","spunk","tit","tosser","turd","twat","vagina","wank","whore","wtf"};
+    public static final CharSequence[] censored = {"anal","anus","arse","ass","ballsack","balls","bastard","bitch","biatch","bloody","blowjob","blow job","bollock","bollok","boner","boob","bugger","bum","butt","buttplug","clitoris","cock","coon","crap","cunt","damn","dick","dildo","dyke","fag","feck","fellate","fellatio","felching","fuck","f u c k","fudgepacker","fudge packer","flange","goddamn","god damn","hell","homo","jerk","jizz","knobend","knob end","labia","lmao","lmfao","muff","nigger","nigga","omg","penis","piss","poop","prick","pube","pussy","queer","scrotum","sex","shit","s hit","sh1t","slut","smegma","spunk","tit","tosser","turd","twat","vagina","wank","whore","wtf"};
     public static final int MAX_NAME_LENGTH = 11;
 
     private NameFormatter() {}
 
     public static String format(String name) {
         for (CharSequence c : censored) {
-            if(name.contains(c)) {
+            if(name.toLowerCase().contains(c)) {
                 name = "sunshine";
             }
         }
