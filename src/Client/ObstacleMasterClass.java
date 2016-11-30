ae5eb50fb4398c39018ba2fbc9f84415a5a39948
Tim2162286 <tjcuprak@gmail.com>
2016-11-29 21:37:24 -0700

    Added javadoc coments

--- src/Client/ObstacleMasterClass.java
+++ src/Client/ObstacleMasterClass.java
@@ -4,7 +4,11 @@ import java.awt.*;
 import java.util.Random;
 
 /**
- * Created by Tim on 10/24/2016.
+ * ObstacleMasterClass is an abstract class used as a base for creating obstacles of various types.
+ * Additionally contains code for generating the common fields used by all obstacle types.
+ *
+ * @author Tim
+ * @since 10/24/2016.
  */
 public abstract class ObstacleMasterClass {
     protected Graphics2D g2;
@@ -21,8 +25,14 @@ public abstract class ObstacleMasterClass {
     protected final int xVel = 250;
     private int backup = 400;
 
-    public boolean remove(){return (xPos+width)<0;}
-
+    /**
+     * Constructor used to generate fields coman to any obstacle type, including x position of the obstacle,
+     * width of obstacle, and size and position of the opening for the bird to fly through.
+     * @param random The random number generator to be used for obstacle generation.
+     * @param windowHeight The Height of the game window.
+     * @param bottomHeight The height of the space used at the bottom of the game window.
+     * @param updateDelay The amount of time in milliseconds between game updates.
+     */
     public ObstacleMasterClass(Random random, int windowHeight, int bottomHeight, int updateDelay){
         HEIGHT = windowHeight;
         BOTTOM_HEIGHT = bottomHeight;
@@ -38,16 +48,40 @@ public abstract class ObstacleMasterClass {
 
     }
 
+    /**
+     * Checks if the obstacle should be removed
+     * @return true if it should be removed, else false.
+     */
+    public boolean remove(){return (xPos+width)<0;}
+
+    /**
+     * Paint method prototype, must be implemented in derived obstacle class.
+     * @param graphics The Graphics2D object to be used in the paint method.
+     */
     public abstract void paint(Graphics2D graphics);
 
+    /**
+     * Changes the x position of the obstacle object in a normal game tick,
+     * also updates lastXPos for generation of future obstacles.
+     * @param numObstacles The number of obstacles that will be updated in current tick.
+     *                     Should be the size of the current obstacle list.
+     */
     public void update(int numObstacles){
         xPos -=((double)UPDATE_DELAY/1000)*xVel;
         lastXPos -=(((double)UPDATE_DELAY/1000)*xVel)/numObstacles;
     }
-    public int getWidth(){return width;}
 
+    /**
+     * Method prototype for collision detection to be implemented in derived obstacle classes.
+     * @param bird The shape to check collision on, should be the bird.
+     * @return Boolean flag, true if collided, false if not.
+     */
     public abstract boolean isCollided(Shape bird);
 
+    /**
+     * Backs the obstacle up by the width of the game window, should be called on all
+     * obstacles currently in existence whenever a collision is detected.
+     */
     public void reset(){
         xPos += backup;
         lastXPos += backup/4;
--- src/Client/SquareObstacle.java
+++ src/Client/SquareObstacle.java
@@ -6,33 +6,65 @@ import java.awt.geom.Rectangle2D;
 import java.util.Random;
 
 /**
- * Created by Tim on 11/6/2016.
+ * SquareObstacle is derived from ObstacleMasterClass, and  creates an obstacle consisting of 2
+ * rectangles aligned vertically, separated by the opening.
+ * @author Tim
+ * @since 11/6/2016.
  */
 public class SquareObstacle extends ObstacleMasterClass {
     private Rectangle2D topShape;
     private Rectangle2D bottomShape;
+
+    /**
+     * Constructor, adds the the top and bottom rectangles which creates the physical obstacle.
+     * @param random The random number generator to be used for obstacle generation.
+     * @param windowHeight The Height of the game window.
+     * @param bottomHeight The height of the space used at the bottom of the game window.
+     * @param updateDelay The amount of time in milliseconds between game updates.
+     */
     public SquareObstacle(Random random, int windowHeight, int bottomHeight, int updateDelay){
         super(random, windowHeight, bottomHeight, updateDelay);
         topShape = new Rectangle2D.Double(super.xPos,0,super.width, super.topHeight);
         bottomShape = new Rectangle2D.Double(super.xPos,super.bottomHeight,super.width,super.HEIGHT-super.BOTTOM_HEIGHT-super.bottomHeight);
     }
 
+    /**
+     * Paints the top and bottom shapes on the game window.
+     * @param graphics The Graphics2D object to be used in the paint method.
+     */
     @Override
     public void paint(Graphics2D graphics) {
 
         graphics.fill(topShape);
         graphics.fill(bottomShape);
     }
+
+    /**
+     * Changes the x position of the obstacle object in a normal game tick,
+     * also updates lastXPos for generation of future obstacles.
+     * @param numObstacles The number of obstacles that will be updated in current tick.
+     */
     @Override
-    public void update(int namObstacles) {
-        super.update(namObstacles);
+    public void update(int numObstacles) {
+        super.update(numObstacles);
         topShape.setFrame(super.xPos,0,super.width, super.topHeight);
         bottomShape.setFrame(super.xPos,super.bottomHeight,super.width,super.HEIGHT-super.BOTTOM_HEIGHT-super.bottomHeight);
     }
+
+    /**
+     * Checks if obstacle should be removed.
+     * @return true if obstacle should be removed, else false.
+     */
     public boolean remove(){
         return super.remove();
     }
 
+    /**
+     * Collision detection between the shapes comprising the obstacle and the birds shape.
+     * @param bird The shape to check collision on, should be the bird.
+     * @return true if collided, else false.
+     */
+    @Override
     public boolean isCollided(Shape bird) {
         Area areaA = new Area(bird);
         areaA.intersect(new Area(topShape));
@@ -40,5 +72,10 @@ public class SquareObstacle extends ObstacleMasterClass {
         areaB.intersect(new Area(bottomShape));
         return !(areaB.isEmpty() && areaA.isEmpty());
     }
+
+    /**
+     * Backs the obstacle up by the width of the game window, should be called on all
+     * obstacles currently in existence whenever a collision is detected.
+     */
     public void reset(){super.reset();}
 }
